25994184addf03fc5add5e5c8ac59e5f361f41c1
Joey Paffel <joey@hygoon.com>
2015-07-16 18:21:31 -0500

    Adwrangler trimmed
    Trimmed bloated listeners and objects, cleaned identifying information from config files.

--- project/src/adwrangler.js
+++ project/src/adwrangler.js
@@ -7,44 +7,25 @@ Ptero.adwrangler = (function() {
     //multiplatform default configuration
     service.configure({
         ios: {
-            interstitial:"dd4f3801738b47b9ac99161d6f6c9de2",
-            interstitialIpad:"7f2db51d87484d56a28f3b42673de672",
+            interstitial:"use your mopub info",
+            interstitialIpad:"use your mopub info",
         },
         android: {
-            interstitial:"8ad5a21723f145e3bb6f56f2baa096de"
+            interstitial:"use your mopub info"
         }
     });
-
-    var adState = {
-        isHidden: false,
-        ctx:null,
-        fullScreenAdvertisement : null,
-        fullScreenAlreadyDownloaded: false,
-        params: {
-            fullscreen : {
-                "status" : null
-            }
-        }
-    };
 
     function init() {createAdListeners(); Cocoon.Ad.loadInterstitial();}
 
     createAdListeners = function(){
         Cocoon.Ad.interstitial.on("ready", function(){
-            adState.fullScreenAlreadyDownloaded = true;
-            adState.params.fullscreen.status = "Full screen ready,";
-            adState.params.fullscreen.sub_status = "press SHOW FULL SCREEN to watch the ad.";
             console.log("interstitial on deck");
         });
         Cocoon.Ad.interstitial.on("shown", function(){
-            adState.params.fullscreen.status = "onFullScreenShown";
-            adState.params.fullscreen.sub_status = "";
             console.log("onFullScreenShown");
         });
         Cocoon.Ad.interstitial.on("hidden", function(){
             console.log("onFullScreenHidden");
-            adState.params.fullscreen.status = "Full screen hidden,";
-            adState.params.fullscreen.sub_status = "press CACHE AD to download another ad.";
         });
 
     };
